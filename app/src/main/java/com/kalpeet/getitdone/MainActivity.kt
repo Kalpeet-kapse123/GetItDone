01f73b97cbb95431c46cd311442a41bccbda625b
Kalpeet-kapse123 <kalpeetkapse23@gmail.com>
2024-04-17 22:37:24 +0530

    GetItDone

---
+++ app/src/main/java/com/kalpeet/getitdone/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.kalpeet.getitdone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.tabs.TabLayoutMediator
+import com.kalpeet.getitdone.databinding.ActivityMainBinding
+import com.kalpeet.getitdone.databinding.DialogAddTaskBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.viewPager.adapter = PagerAdapter(this)
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = "Tasks"
+        }.attach()
+
+        binding.floatingActionButton.setOnClickListener {
+            val dialogBinding = DialogAddTaskBinding.inflate(layoutInflater)
+            MaterialAlertDialogBuilder(this).setTitle("Add new Task").setView(dialogBinding.root)
+                .setPositiveButton("Save") { _, _ ->
+                    Toast.makeText(
+                        this,
+                        "Your task is: ${dialogBinding.editText.text}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                .setNegativeButton("Cancel", null)
+                .show()
+
+        }
+    }
+
+    inner class PagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+        override fun getItemCount() = 1
+
+        override fun createFragment(position: Int): Fragment {
+            return TasksFragment()
+        }
+
+
+    }
+
+}
